5eb45159ef5d9594b0da57e31f17e7c6aa9e95ff
Jasvinder Singh <jasvindersingh01@gmail.com>
2025-12-08 10:40:33 +0530

    Remove Base

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import App from './App.jsx'
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/asterenergy">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 )
